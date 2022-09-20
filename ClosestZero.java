12dac9f2a7c8bcde96f65a0b6325a850fdf6fe00
Mike Koutelieris <mixoskout@gmail.com>
2022-09-20 23:34:06 +0300

    Create ClosestZero.java

---
+++ ClosestZero.java
@@ -0,0 +1,55 @@
+import java.util.*;
+import java.lang.Math.*;
+
+public class ClosestZero {
+
+  public static int closeToZero(int[] temps) {
+
+    int closest = 0;
+    ArrayList<Integer> tempsM = new ArrayList<Integer>();
+    ArrayList<Integer> tempsP = new ArrayList<Integer>();
+
+    for(int i = 0; i < temps.length; i++) {
+      if(temps[i] == 0) {
+        closest = 0;
+        return closest;
+      } else if(temps[i] < 0) {
+        tempsM.add(temps[i]);
+      } else if(temps[i] > 0) {
+        tempsP.add(temps[i]);
+      }
+    }
+
+    Collections.sort(tempsM);
+    Collections.sort(tempsP);
+    
+    if(Math.abs(tempsM.get(tempsM.size()-1)) == tempsP.get(0)) {
+      closest = tempsP.get(0);
+    } else if(Math.abs(tempsM.get(tempsM.size()-1)) > tempsP.get(0)) {
+      closest = tempsP.get(0);
+    } else if(Math.abs(tempsM.get(tempsM.size()-1)) < tempsP.get(0)) {
+      closest = tempsM.get(tempsM.size()-1);
+    }
+    
+    return closest;
+
+  }
+
+  public static void main(String[] args) {
+    
+    int[] test1 = new int[]{1, -3, 0, 45, -56};
+    int[] test2 = new int[]{4};
+    int[] test3 = new int[]{-2, -45, -90};
+    int[] test4 = new int[]{-2, 2, 90, -80, -45};
+    
+    System.out.println(Arrays.toString(test1) + "\n");
+    System.out.println("Closest temperature to 0 for the array above is: " + closeToZero(test1) + "\n");
+    System.out.println(Arrays.toString(test2) + "\n");
+    System.out.println("Closest temperature to 0 for the array above is: " + closeToZero(test2) + "\n");
+    System.out.println(Arrays.toString(test3) + "\n");
+    System.out.println("Closest temperature to 0 for the array above is: " + closeToZero(test3) + "\n");
+    System.out.println(Arrays.toString(test4) + "\n");
+    System.out.println("Closest temperature to 0 for the array above is: " + closeToZero(test4) + "\n");
+
+  }
+}
